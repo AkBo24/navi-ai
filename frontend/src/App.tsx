312caf244cf9c25a2eedd59eb3a12177faf604d8
Akshay Bodla <akshaybodla21@gmail.com>
2024-11-23 20:12:42 -0800

    Rebuilding project to serve react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Example = (props: { title: string; number: number }) => {
+    return (
+        <div>
+            {props.title} {props.number}
+        </div>
+    );
+};
+
+const App = () => {
+    return <Example title='Hello World' number={23933} />;
+};
+
+export default App;
